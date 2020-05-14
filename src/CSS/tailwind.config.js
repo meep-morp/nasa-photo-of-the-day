a30deda810cdca0b98d2407aeab349f0faa1cf33
meep-morp <gracieaerogirl@gmail.com>
2020-05-14 16:45:12 -0500

    forgot to add again

---
+++ src/CSS/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    colors: {
+      black: 'rgb(0,0,0)',
+      blackGrad: 'rgba(0,0,0,1)',
+      midBlueGrad: 'rgba(10,34,88,1)',
+      lightBlueGrad: 'rgba(30,79,156,1)',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
